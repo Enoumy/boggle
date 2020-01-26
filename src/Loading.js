bed6d684f72bc6dc46d11396eccd2ef92640c3b2
Enoumy <enoumy@gmail.com>
2020-01-25 17:43:38 -0800

    Added lloading widget

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
+    textAlign: 'center',
+    margin: '0 auto',
     display: 'flex',
     '& > * + *': {
       marginLeft: theme.spacing(2),
@@ -15,8 +18,12 @@ export default function CircularIndeterminate() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CircularProgress />
-    </div>
+    <Grid container justify="center">
+      <Grid item>
+        <div className={classes.root}>
+          <CircularProgress />
+        </div>
+      </Grid>
+    </Grid>
   );
 }
