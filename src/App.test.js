1b9675f9577e8e45bde60fdaa37bd6507205a0fe
Enoumy <enoumy@gmail.com>
2020-01-29 19:10:00 -0800

    Added starting visual tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('Main app tests', () => {
+  test('Navbar title', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Boggle Game/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('Renders start button on load', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/START/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
