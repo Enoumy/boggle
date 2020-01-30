35258f8d42900448ed14ee322db943cde4340291
Enoumy <enoumy@gmail.com>
2020-01-29 21:34:53 -0800

    Updated index and manifest

--- src/App.test.js
+++ src/App.test.js
@@ -3,15 +3,17 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('Main app tests', () => {
-  test('Navbar title', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/Boggle Game/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+  describe('Static tests', () => {
+    test('Navbar title', () => {
+      const { getByText } = render(<App />);
+      const linkElement = getByText(/Boggle Game/i);
+      expect(linkElement).toBeInTheDocument();
+    });
 
-  test('Renders start button on load', () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText(/START/i);
-    expect(linkElement).toBeInTheDocument();
+    test('Renders start button on load', () => {
+      const { getByText } = render(<App />);
+      const linkElement = getByText(/START/i);
+      expect(linkElement).toBeInTheDocument();
+    });
   });
 });
