6506809fa9806662d88111b7c5d5f9fd1eb0bfaa
Enoumy <enoumy@gmail.com>
2020-01-29 23:55:40 -0800

    Added start button tests

---
+++ src/StartButton.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import StartButton from './StartButton.js';
+
+test('Stope state works', () => {
+  const state = 'stopped';
+
+  const { getByText } = render(<StartButton state={state} />);
+
+  const element = getByText(/start/i);
+  expect(element).toBeInTheDocument();
+});
+
+test('Stop state works', () => {
+  const state = 'active';
+
+  const { getByText } = render(<StartButton state={state} />);
+
+  const element = getByText(/stop/i);
+  expect(element).toBeInTheDocument();
+});
