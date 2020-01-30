896c62a2de8882e870f9c42608ca520d2aebf7bf
Enoumy <enoumy@gmail.com>
2020-01-29 20:56:57 -0800

    Added wordlist tests

--- src/WordList.test.js
+++ src/WordList.test.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
-
+import { render } from '@testing-library/react';
 import WordList from './WordList.js';
 
-let container = null;
-beforeEach(() => {
-  // Sets up a DOM element as render target.
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // Cleanup
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
 test('Renders correct title', () => {
-  act(() => {
-    render(<WordList title="Game of Thrones" words={['']} />, container);
-  });
+  // Tests that the WordList is rendered with the correct title.
+  const title = 'Game of Thrones';
+  const words = [''];
+
+  const { getByText } = render(<WordList title={title} words={words} />);
+
+  const element = getByText(/Game of Thrones/i);
+  expect(element).toBeInTheDocument();
+});
+
+test('Renders correct word list', () => {
+  // Tests that the WordList renders the correct word list.
+  const title = '';
+  const words = ['Ravenclaw', 'Hufflepuff', 'Griffindor'];
+
+  const { getByText } = render(<WordList title={title} words={words} />);
+
+  let element;
+  element = getByText(/Ravenclaw/i);
+  expect(element).toBeInTheDocument();
+  element = getByText(/Hufflepuff/i);
+  expect(element).toBeInTheDocument();
+  element = getByText(/Griffindor/i);
+  expect(element).toBeInTheDocument();
 });
