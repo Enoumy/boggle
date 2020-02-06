5cb4de0da1825a283c3bea11e780240a418203a8
Enoumy <enoumy@gmail.com>
2020-02-06 09:56:47 -0800

    Added not logged in alert

--- src/ChallengeGameMenu.js
+++ src/ChallengeGameMenu.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import UserResponse from './UserResponse.js';
 import AvailableChallenges from './AvailableChallenges.js';
+import Alert from '@material-ui/lab/Alert';
 
 function ChallengeGameMenu(props) {
   return (
     <div>
+      {props.loggedIn ? (
+        <div></div>
+      ) : (
+        <div style={{ margingBottom: 8 }}>
+          <Alert severity="info">Log-in to record your high-score!</Alert>
+        </div>
+      )}
       <AvailableChallenges user={props.user} loggedIn={props.loggedIn} />
     </div>
   );
