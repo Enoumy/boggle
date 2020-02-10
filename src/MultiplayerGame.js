64abdce6e518a8c274c4d00a53361f4b6db4d7c4
Enoumy <enoumy@gmail.com>
2020-02-09 17:29:43 -0800

    Added normal use states

--- src/MultiplayerGame.js
+++ src/MultiplayerGame.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
+import copy from 'copy-to-clipboard';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
 function MultiplayerGame(props) {
+  const [host, setHost] = useState('');
+  const [board, setBoard] = useState([[]]);
+  const [wordsFoundAll, setWordsFoundAll] = useState({});
+  const [gameState, setGameState] = useState('not-found');
+
   let { game } = useParams();
 
   return (
-    <Typography variant="h6" align="center">
-      Multiplayer Game {game}
-    </Typography>
+    <div>
+      <Typography variant="h6" align="center">
+        Multiplayer Game {game}
+      </Typography>
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <Paper style={{ padding: 8 }}>
+          <Typography variant="h7">Game code: {game}</Typography>
+          <Button
+            variant="contained"
+            style={{ marginLeft: 8 }}
+            onClick={() => {
+              copy(game);
+            }}
+          >
+            Copy
+          </Button>
+        </Paper>
+        <Paper style={{ padding: 8 }}>
+          <Typography variant="h7">Game code: {host}</Typography>
+        </Paper>
+      </Grid>
+    </div>
   );
 }
 
