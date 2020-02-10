ca40413c8c18810c8f15a5cf06c3859f7beea46b
Enoumy <enoumy@gmail.com>
2020-02-09 17:53:43 -0800

    Implemented functionality for joining a game

--- src/MultiplayerGame.js
+++ src/MultiplayerGame.js
@@ -5,9 +5,10 @@ import copy from 'copy-to-clipboard';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
 
 function MultiplayerGame(props) {
-  const [host, setHost] = useState('');
+  const [host, setHost] = useState('dummyHost');
   const [board, setBoard] = useState([[]]);
   const [wordsFoundAll, setWordsFoundAll] = useState({});
   const [gameState, setGameState] = useState('not-found');
@@ -16,31 +17,37 @@ function MultiplayerGame(props) {
 
   return (
     <div>
-      <Typography variant="h6" align="center">
-        Multiplayer Game {game}
-      </Typography>
-      <Grid
-        container
-        direction="column"
-        justify="flex-start"
-        alignItems="center"
-      >
-        <Paper style={{ padding: 8 }}>
-          <Typography variant="h7">Game code: {game}</Typography>
-          <Button
-            variant="contained"
-            style={{ marginLeft: 8 }}
-            onClick={() => {
-              copy(game);
-            }}
+      {!props.loggedIn ? (
+        <Alert severity="info">Log-in to play multiplayer!</Alert>
+      ) : (
+        <div>
+          <Typography variant="h6" align="center">
+            Multiplayer Game {game}
+          </Typography>
+          <Grid
+            container
+            direction="column"
+            justify="flex-start"
+            alignItems="center"
           >
-            Copy
-          </Button>
-        </Paper>
-        <Paper style={{ padding: 8 }}>
-          <Typography variant="h7">Game code: {host}</Typography>
-        </Paper>
-      </Grid>
+            <Paper style={{ padding: 8 }}>
+              <Typography variant="h7">Game code: {game}</Typography>
+              <Button
+                variant="contained"
+                style={{ marginLeft: 8 }}
+                onClick={() => {
+                  copy(game);
+                }}
+              >
+                Copy
+              </Button>
+            </Paper>
+            <Paper style={{ padding: 8, marginTop: 8 }}>
+              <Typography variant="h7">Host: {host}</Typography>
+            </Paper>
+          </Grid>
+        </div>
+      )}
     </div>
   );
 }
--- src/MultiplayerGameMenu.js
+++ src/MultiplayerGameMenu.js
@@ -7,6 +7,8 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import SizeSelector from './SizeSelector.js';
+import Alert from '@material-ui/lab/Alert';
+import { Redirect } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -18,50 +20,75 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+let selectedGame = '';
+
 function MultiplayerGameMenu(props) {
   const [size, setSize] = useState(4);
+  const [redirected, setRedirected] = useState(false);
+  const [currentGame, setCurrentGame] = useState('');
 
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.root} justify="center" spacing={2}>
-      <Grid item>
-        <Typography variant="h6" align="center">
-          Create a game
-        </Typography>
-        <Paper className={classes.paper}>
-          <SizeSelector parentCallback={setSize} />
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Button variant="contained" color="primary" align="center">
-              Create Game
-            </Button>
-          </div>
-        </Paper>
-      </Grid>
-      <Grid item>
-        <Typography variant="h6" align="center">
-          Join a game
-        </Typography>
-        <Paper className={classes.paper}>
-          <Grid container>
-            <TextField
-              id="outlined-basic"
-              label="Enter game code"
-              variant="outlined"
-            />
-            <Button variant="outlined" color="primary">
-              Join
-            </Button>
+    <div>
+      {redirected ? (
+        <Redirect to={'/multiplayer/' + currentGame} />
+      ) : (
+        <div></div>
+      )}
+
+      {!props.loggedIn ? (
+        <Alert severity="info">Log-in to play multiplayer!</Alert>
+      ) : (
+        <Grid container className={classes.root} justify="center" spacing={2}>
+          <Grid item>
+            <Typography variant="h6" align="center">
+              Create a game
+            </Typography>
+            <Paper className={classes.paper}>
+              <SizeSelector parentCallback={setSize} />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Button variant="contained" color="primary" align="center">
+                  Create Game
+                </Button>
+              </div>
+            </Paper>
           </Grid>
-        </Paper>
-      </Grid>
-    </Grid>
+          <Grid item>
+            <Typography variant="h6" align="center">
+              Join a game
+            </Typography>
+            <Paper className={classes.paper}>
+              <Grid container>
+                <TextField
+                  id="outlined-basic"
+                  label="Enter game code"
+                  variant="outlined"
+                  onChange={event => {
+                    setCurrentGame(event.target.value);
+                  }}
+                />
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => {
+                    if (currentGame !== '') setRedirected(true);
+                  }}
+                >
+                  Join
+                </Button>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      )}
+    </div>
   );
 }
 
